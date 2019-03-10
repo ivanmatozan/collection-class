957d3bf19aee7ae22ff09a9a5288aa3e72a9a1b5
Ivan Matozan <ivan.matozan@gmail.com>
2019-03-10 14:49:01 +0100

    Create DB structure

---
+++ db.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS collection
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+USE collection;
+
+CREATE TABLE IF NOT EXISTS article (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Id',
+	title VARCHAR(255) NOT NULL COMMENT 'Title',
+	body TEXT NOT NULL COMMENT 'Body',
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Create At',
+	CONSTRAINT PRIMARY KEY (id)
+);
+
+INSERT INTO article (title, body) VALUES
+  ('Article 1', 'Article Body 1'),
+  ('Article 2', 'Article Body 2'),
+  ('Article 3', 'Article Body 3'),
+  ('Article 4', 'Article Body 4');
